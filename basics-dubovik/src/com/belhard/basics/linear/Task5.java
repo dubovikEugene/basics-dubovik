fc80493bba9296d4bf86841d78a685c13e1938a1
Eugene Dubovik <dev.dubovik@gmail.com>
2021-11-21 15:07:15 +0300

    changed linear.Task5 and task6

--- basics-dubovik/src/com/belhard/basics/linear/Task5.java
+++ basics-dubovik/src/com/belhard/basics/linear/Task5.java
@@ -2,19 +2,32 @@ package com.belhard.basics.linear;
 
 import java.util.Scanner;
 
+import com.belhard.basics.util.ConsoleUtil;
+import com.belhard.basics.util.ResultPrinter;
+
 public class Task5 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please, enter x");
-		double x = scanner.nextDouble();
-		System.out.println("Please, enter y");
-		double y = scanner.nextDouble();
+		System.out.println("let`s try solve expression \n\t");
+		System.out.println("((sin(x) + cos(y)) / (cos(x) - sin(y)) * tan(x * y)\n");
 
-		double result = ((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y))) * Math.tan(x * y);
-		System.out.println(result);
-
-		scanner.close();
+		double x = ConsoleUtil.GetDoubleFromConsole(scanner, "Please, enter x in degree -->\t");
+		double y = ConsoleUtil.GetDoubleFromConsole(scanner, "Please, enter y in degree -->\t");
+		
+		double radX = Math.toRadians(x);
+		double radY = Math.toRadians(y);
+		
+		double numerator = Math.sin(radX) + Math.cos(radY);
+		double denominator = Math.cos(radX) - Math.sin(radY);
+		
+		double result = (numerator / denominator) * Math.tan(radX * radY);
+		double degResult = Math.toDegrees(result);
+		if (denominator != 0) {
+			ResultPrinter.DoubleResultToConsole(degResult);
+		} else {
+			System.out.println("it is impossible to solve the equation");
+		}
 
 	}
 

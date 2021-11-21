e9d1c35c0544db2ae51513f9fef8667be6f25017
Eugene Dubovik <dev.dubovik@gmail.com>
2021-11-21 15:34:17 +0300

    changed cycles.Task1

--- basics-dubovik/src/com/belhard/basics/cycles/Task1.java
+++ basics-dubovik/src/com/belhard/basics/cycles/Task1.java
@@ -2,26 +2,19 @@ package com.belhard.basics.cycles;
 
 import java.util.Scanner;
 
+import com.belhard.basics.util.ConsoleUtil;
+import com.belhard.basics.util.ResultPrinter;
+
 public class Task1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please, enter positive integer");
-		int result = 0;
-		if (scanner.hasNextInt()) {
-			int num = scanner.nextInt();
-			if (num > 0) {
-				result = (1 + num) * num / 2;
-//				for ( int i = 1; i <= num; i++ ) {
-//					result += i;
-//				} 
-			} else {
-				System.out.println("It`s not positive integer");
-			}
-
+		System.out.println("Now let`s calculate the sum of all positive numbers up to the entered\n");
+		int num = ConsoleUtil.GetIntFromConsole(scanner, "Please, enter positive numbre -->\t");
+		if (num > 0) {
+			int result = (1 + num) * num / 2;
+			ResultPrinter.IntResultToConsole(result);
 		} else {
-			System.out.println("It`s not integer, try again");
+			System.out.println("It`s not positive integer");
 		}
-		System.out.println("Sum of all positive numbers up to given " + result);
-		scanner.close();
 	}
 }

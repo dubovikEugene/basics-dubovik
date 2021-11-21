f02305a44f217615f2d1b27da83bf0ade01c7a55
Eugene Dubovik <dev.dubovik@gmail.com>
2021-11-21 15:45:08 +0300

    changed cycles.Task2

--- basics-dubovik/src/com/belhard/basics/cycles/Task2.java
+++ basics-dubovik/src/com/belhard/basics/cycles/Task2.java
@@ -2,26 +2,29 @@ package com.belhard.basics.cycles;
 
 import java.util.Scanner;
 
+import com.belhard.basics.util.ConsoleUtil;
+import com.belhard.basics.util.ResultPrinter;
+
 public class Task2 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please, enter two integer to indicate a segment");
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		System.out.println("Please, enter step");
-		int h = scanner.nextInt();
-		int x;
-		int y;
-		scanner.close();
 
-		for (x = a; x < b; x += h) {
+		System.out.print("Let`s calculate the value of the function on segment [a,b] with step h");
+		System.out.print("\n\n\ty = x| if x > 2\n\tor\n\ty = -x| if x <= 2\n\n");
+		int a = ConsoleUtil.GetIntFromConsole(scanner, "Please, enter a -->\t");
+		int b = ConsoleUtil.GetIntFromConsole(scanner, "Please, enter b -->\t");
+		int step = ConsoleUtil.GetIntFromConsole(scanner, "Please, enter step -->\t");
+		int x;
+		int result;
+
+		for (x = a; x < b; x += step) {
 			if (x > 2) {
-				y = x;
-				System.out.println(y);
+				result = x;
+				ResultPrinter.IntResultToConsole(result);
 			} else {
-				y = -x;
-				System.out.println(y);
+				result = -x;
+				ResultPrinter.IntResultToConsole(result);
 			}
 		}
 

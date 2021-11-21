ea0b4f404a7b87dd0d2fc8be1789864d3a17ea87
Eugene Dubovik <dev.dubovik@gmail.com>
2021-11-21 14:36:14 +0300

    changed ConsoleUtil

--- basics-dubovik/src/com/belhard/basics/util/ConsoleUtil.java
+++ basics-dubovik/src/com/belhard/basics/util/ConsoleUtil.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class ConsoleUtil {
 	public static int GetIntFromConsole(Scanner scanner, String prompt) {
 		System.out.print(prompt);
+		int counter = 4;
 		while (!scanner.hasNextInt()) {
 			scanner.next();
-			System.out.println("It`s not integer, try again");
+			counter--;
+			if (counter != 0) {
+				System.out.print("It`s not integer, try again you have " + counter + " tries -> \t");
+
+			} else {
+				System.out.println("Sorry, try next time");
+				System.exit(1);
+			}
 		}
 		int intFromConsole = scanner.nextInt();
 		return intFromConsole;
@@ -16,15 +24,16 @@ public class ConsoleUtil {
 
 	public static double GetDoubleFromConsole(Scanner scanner, String prompt) {
 		System.out.print(prompt);
-		int counter = 3;
+		int counter = 4;
 		while (!scanner.hasNextDouble()) {
+			scanner.next();
+			counter--;
 			if (counter != 0) {
-				scanner.next();
-				System.out.print("It`s not number, try again, you have " + counter + " tries -> \t");
-				counter--;
+				System.out.print("It`s not integer, try again you have " + counter + " tries -> \t");
+				
 			} else {
-				System.out.println("Ok, set the defoul value = 0");
-				return 0;
+				System.out.println("Sorry, try next time");
+				System.exit(1);
 			}
 		}
 		double doubleFromConsole = scanner.nextDouble();

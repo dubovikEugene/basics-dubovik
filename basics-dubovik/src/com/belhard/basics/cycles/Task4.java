384bdb40287d7fd725ff3c88821a98b78549c815
Eugene Dubovik <dev.dubovik@gmail.com>
2021-11-21 16:10:21 +0300

    changed cycles.Task4, cycles.Task7

--- basics-dubovik/src/com/belhard/basics/cycles/Task4.java
+++ basics-dubovik/src/com/belhard/basics/cycles/Task4.java
@@ -5,8 +5,8 @@ public class Task4 {
 	public static void main(String[] args) {
 		System.out.println("\n\t\t\t\tACSII\n");
 		for (int i = 32; i <= 127; i++) {
-			System.out.print("\t" + i +  " is " + "[ " +  (char) i +" ]");
-			if ((i-31)%4 == 0) {
+			System.out.print("\t" + i + " is " + "[ " + (char) i + " ]");
+			if ((i - 31) % 4 == 0) {
 				System.out.println();
 			}
 		}
--- basics-dubovik/src/com/belhard/basics/cycles/Task7.java
+++ basics-dubovik/src/com/belhard/basics/cycles/Task7.java
@@ -2,18 +2,20 @@ package com.belhard.basics.cycles;
 
 import java.util.Scanner;
 
+import com.belhard.basics.util.ConsoleUtil;
+
 public class Task7 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Insert the extremes of the gap");
-		int m = scanner.nextInt();
-		int n = scanner.nextInt();
+		System.out.println("\tThe program displays the divisors for each natural number in the range from m to n\n");
+		int intervalStart = ConsoleUtil.GetIntFromConsole(scanner, "Enter interval start ->\t");
+		int intervalEnd = ConsoleUtil.GetIntFromConsole(scanner, "Enter interval end ->\t");
 
-		for (int i = m; i <= n; i++) {
+		for (int i = intervalStart; i <= intervalEnd; i++) {
 			for (int j = 2; j <= i; j++) {
 				if (i % j == 0 && i != j) {
-					System.out.println(i + " have devidors ->\t" + j);
+					System.out.println(i + " have devidor ->\t" + j);
 				}
 			}
 		}

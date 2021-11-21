785ba8e34be7aa18f15d2e812195e835741c3535
Eugene Dubovik <dev.dubovik@gmail.com>
2021-11-21 15:11:14 +0300

    changed linear.Task6

--- basics-dubovik/src/com/belhard/basics/linear/Task6.java
+++ basics-dubovik/src/com/belhard/basics/linear/Task6.java
@@ -2,19 +2,20 @@ package com.belhard.basics.linear;
 
 import java.util.Scanner;
 
+import com.belhard.basics.util.ConsoleUtil;
+
 public class Task6 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please, enter x");
-		int x = scanner.nextInt();
-		System.out.println("Please, enter y");
-		int y = scanner.nextInt();
+		System.out.println("find out if your point lies in the shaded area\n");
+		
+		int x = ConsoleUtil.GetIntFromConsole(scanner, "Please, enter X -->\t");
+		int y = ConsoleUtil.GetIntFromConsole(scanner, "Please, enter Y -->\t");
 
 		if (x >= -4 && x <= 4 && x != 0 && y != -1 && y >= -3 && y <= 0 || x >= -2 && x <= 2 && y >= -3 && y <= 4) {
 			System.out.println(true);
 		} else {
 			System.out.println(false);
 		}
-		scanner.close();
 	}
 }

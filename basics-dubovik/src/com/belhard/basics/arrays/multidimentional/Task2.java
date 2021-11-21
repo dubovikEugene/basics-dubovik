f29f3f2f6b727c145d98a76d20cf505c959ddf68
Eugene Dubovik <dev.dubovik@gmail.com>
2021-11-21 18:46:13 +0300

    create array.multidimentional.Task2

---
+++ basics-dubovik/src/com/belhard/basics/arrays/multidimentional/Task2.java
@@ -0,0 +1,11 @@
+package com.belhard.basics.arrays.multidimentional;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		int[][] array = { { 0, 1, 2, 3, 12 }, { 12, 15, 16, 17 }, { 44, 54, 82, 90 }, { 7, 7, 7, 7 } };
+		
+		
+	}
+
+}
